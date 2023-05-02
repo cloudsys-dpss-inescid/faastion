2d553cc57205232d0e454aea36e2e7f205798056
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-05-02 17:07:06 -0400

    Adding a helper script to run benchmarks.

---
+++ native-execution/run-benchmarks.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+JAVA_AGENT=$DIR/javassist/target/JavassistWrapper-1.0-jar-with-dependencies.jar
+TOOL="MethodExecutionTimer"
+
+function build_javassist_agent {
+    cd $DIR/javassist
+    mvn package
+    cd - &> /dev/null
+}
+
+function build_benchmark {
+    cd $DIR/benchmarks/$benchmark
+    ./build.sh
+    cd - &> /dev/null
+}
+
+#build_javassist_agent
+
+for benchmark in classify filehashing helloworld httprequest videoprocessing
+do
+    #build_benchmark
+
+    BUILD=$DIR/benchmarks/$benchmark/build
+    MANIFEST=${BUILD}/tmp/shadowJar/MANIFEST.MF
+    CLASS_PATH=(${BUILD}/libs/*all.jar)
+
+    # Replace * with the actual package name
+    class_path=$(echo "${CLASS_PATH[*]}")
+    entrypoint=$(grep -i 'Main-Class' ${MANIFEST} | awk -F ': ' '{print $2}')
+
+    # Get all directories with a *.class file in them and replace '/' with '.'
+    output=$(jar -tf ${class_path} | grep -oE ".*\/[^/]+\.class" | sed 's#/[^/]*$##; s#/#.#g' | sort -u)
+    # String of packages separated by commas
+    printf -v packages '%s,' $output
+    packages=${packages%,}
+
+    $DIR/native-benchmark.py -c "java -cp ${class_path} -javaagent:${JAVA_AGENT}=${TOOL}:${packages}:output ${entrypoint}"
+done
