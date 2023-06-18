8fa6d293ce1f7e64cb919249633ead4641b3c037
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-06-18 11:03:38 +0100

    Updating plots.

--- native-execution/generate-plot.py
+++ native-execution/generate-plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 transitions = np.loadtxt('results/transitions.dat')
 percentages = np.loadtxt('results/percentages.dat')
-labels      = ["Hello World", "File Hashing", "REST", "Microservice\n (Micronaut)", "ML inference\n (TensorFlow)"]
+labels      = ["Hello World", "File Hashing", "REST", "Shopcart\n(Micronaut)", "PetClinic\n(SpringBoot)", "ML inference\n (TensorFlow)"]
 x           = np.arange(len(labels))
 
 #transitions[3]=0
@@ -16,21 +16,24 @@ x           = np.arange(len(labels))
 #percentages[4]=0
 #labels[4]=""
 
+plt.rcParams.update({'font.size': 10})
+plt.rcParams["figure.figsize"] = (8,4)
+
 width = .25
 fig, ax1 = plt.subplots()
 
-ax1.bar(x - (width * 1.05)/2, transitions, width, label="Transitions per sec")
+ax1.bar(x - (width * 1.05)/2, transitions, width, label="Transitions per second")
 ax1.set_xticks(x, labels)
-ax1.set_ylim(ymin=0, ymax=70)
+ax1.set_ylim(ymin=0, ymax=25)
 ax1.set_ylabel("Number of transitions per second")
 ax1.grid(axis = 'y', linestyle = '--', linewidth = 0.25)
 
 ax2 = ax1.twinx()
-ax2.bar(x + (width * 1.05)/2, percentages, width, color="red", label="% Native Code")
+ax2.bar(x + (width * 1.05)/2, percentages, width, color="red", label="% of time in native code")
 ax2.set_ylabel("Percentage of time in native code")
-ax2.set_ylim(ymin=0, ymax=100)
+ax2.set_ylim(ymin=0, ymax=25)
 
-fig.legend(bbox_to_anchor=(.65,.96))
+fig.legend(ncol=2, bbox_to_anchor=(.65,.96))
 plt.tight_layout()
 plt.savefig("native-execution.pdf")
 plt.savefig("native-execution.png", dpi=300)
