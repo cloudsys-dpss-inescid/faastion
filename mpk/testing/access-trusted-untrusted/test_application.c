54fe824770afcd629254b3514b1c3ca90a01eb22
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-07-12 12:27:53 +0100

    Updates to test application.c.

--- mpk/testing/access-trusted-untrusted/test_application.c
+++ mpk/testing/access-trusted-untrusted/test_application.c
@@ -12,40 +12,24 @@
 #include <common.h>
 #include <erim.h>
 
-unsigned long read_var(unsigned long * var) {
-  return *var;
-}
-
-unsigned long * create_secret_var() {
-  // init isolation and sh mem
-  if(erim_init(8192, ERIM_FLAG_ISOLATE_TRUSTED)) {
-    exit(EXIT_FAILURE);
-  }
-  erim_switch_to_trusted;
-
-  // allocate secret
-  unsigned long * var = (unsigned long *) erim_malloc(sizeof(unsigned long));
-  if(var == NULL) {
-    printf("allocation of secret failed\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  return var;
-}
+int inc(int arg) { return arg++; }
 
 int main(int argc, char **argv) {
-  unsigned long * var = create_secret_var();
 
-  printf("var located at %p\n", var);
+  if(erim_init(8192, ERIM_FLAG_ISOLATE_TRUSTED | ERIM_FLAG_SWAP_STACK)) {
+    exit(EXIT_FAILURE);
+  }
+#ifdef ERIM_SWAP_STACKS
+  printf("ERIM_SWAP_STACKS is defined!\n");
+#endif
 
+  ERIM_SWITCH_TO_UNTRUSTED_STACK;
+  __wrpkru(ERIM_UNTRUSTED_PKRU);
 
-  erim_switch_to_untrusted;
-  unsigned long ola = 10;
+  int ret = inc(3);
   
-  // try to read, shouldn't work (not trusted)
-  fprintf(stderr, "should segfault:\n");
-  fprintf(stderr, "var: %lx\n", read_var(var));
-  erim_switch_to_trusted;
+  __wrpkru(ERIM_TRUSTED_PKRU);
+  ERIM_SWITCH_TO_TRUSTED_STACK;
 
-  return SWS_SUCCESS;
+  return 0;
 }
