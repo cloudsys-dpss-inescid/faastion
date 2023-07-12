1564d3048ad04d068e15c439e591e67282f565dc
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-07-12 18:37:20 +0100

    Updating app after adding support for stack swap.

--- mpk/testing/access-trusted-untrusted/test_application.c
+++ mpk/testing/access-trusted-untrusted/test_application.c
@@ -12,24 +12,28 @@
 #include <common.h>
 #include <erim.h>
 
-int inc(int arg) { return arg++; }
+int inc(int a) { return a++; }
+
+void wrapper() {
+  // TODO - extract arguments from shared storage;
+  // TODO - call inc
+  // TODO - insert arguments back in storage;
+}
 
 int main(int argc, char **argv) {
-
   if(erim_init(8192, ERIM_FLAG_ISOLATE_TRUSTED | ERIM_FLAG_SWAP_STACK)) {
     exit(EXIT_FAILURE);
   }
-#ifdef ERIM_SWAP_STACKS
-  printf("ERIM_SWAP_STACKS is defined!\n");
-#endif
-
-  ERIM_SWITCH_TO_UNTRUSTED_STACK;
-  __wrpkru(ERIM_UNTRUSTED_PKRU);
-
-  int ret = inc(3);
-  
   __wrpkru(ERIM_TRUSTED_PKRU);
-  ERIM_SWITCH_TO_TRUSTED_STACK;
+  // TODO - copy in arguments
+
+  ERIM_SWITCH_TO_ISOLATED_STACK;
+  __wrpkru(ERIM_UNTRUSTED_PKRU);
+  wrapper();
+  __wrpkru(ERIM_TRUSTED_PKRU);
+  ERIM_SWITCH_TO_REGULAR_STACK;
+
+  // TODO - copy out arguments
 
   return 0;
 }
