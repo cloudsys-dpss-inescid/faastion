55a79edd0706582dbf1f0c823efb85a8f4751b3b
ist192549 <ist192549@tecnico.ulisboa.pt>
2023-08-21 19:43:56 +0100

    Updated LD_PRELOAD lib.

--- mpk/ld-preload/preload.c
+++ mpk/ld-preload/preload.c
@@ -7,28 +7,21 @@
 
 AppMap appMap;
 ThreadMap threadMap;
-static __thread int no_hook = 0;
-int verbose = 0;  // Set this flag to 1 for logging, or 0 to disable logging
 
 /* Function pointers declarations */
 static int( * real_pthread_create)(pthread_t * , const pthread_attr_t * , void * ( * )(void * ), void * ) = NULL;
 static void( * real_pthread_exit)(void *) = NULL;
 static void * ( * real_dlopen)(const char * , int) = NULL;
-static void * ( * real_mmap)(void *, size_t, int, int, int, off_t) = NULL;
-static int( * real_munmap)(void *, size_t) = NULL;
-static void * ( * real_malloc)(size_t) = NULL;
-static void * ( * real_realloc)(void *, size_t) = NULL;
-static void( * real_free)(void *) = NULL;
 
 /* Constructor */
 static void __attribute__((constructor)) init(void) {
     initAppMap(&appMap);
     initThreadMap(&threadMap);
+
     // Initialize isolation
-    if (erim_init(32768, ERIM_FLAG_ISOLATE_TRUSTED)) {
+    if (erim_init(32768, ERIM_FLAG_ISOLATE_TRUSTED | ERIM_FLAG_SWAP_STACK, 16)) {
         exit(EXIT_FAILURE);
     }
-    erim_switch_to_untrusted;
 }
 
 /* App functions */
@@ -45,6 +38,18 @@ int isEmpty(int domain) {
     return 1;
 }
 
+int findEmptyDomain() {
+    unsigned long index = -1;
+
+    for (int i = 1; i < 16; i++) {
+        index = hash_int(i);
+        if (threadMap.buckets[index] == NULL)
+            return i;
+    }
+    // Domain not found
+    return -1;
+}
+
 void setAppPermissions(const char* id, int protectionFlag, int pkey) {
     size_t count;
     MemoryRegion* regions = getRegions(appMap, (char*)id, &count);
@@ -57,106 +62,7 @@ void setAppPermissions(const char* id, int protectionFlag, int pkey) {
     }
 }
 
-/* Memory allocation and mapping */
-#ifdef MALLOC
-void * malloc(size_t size) {
-    void *ret;
-
-    if (real_malloc == NULL) {
-        real_malloc =  (void *(*) (size_t)) dlsym(RTLD_NEXT, "malloc");
-    }
-
-    if (no_hook) {
-        return (*real_malloc)(size);
-    }
-
-    no_hook = 1;
-    ret = (*erim_malloc)(size);
-    no_hook = 0;
-
-    return ret;
-}
-#endif
-
-#ifdef FREE
-void free(void * ptr) {
-    if (real_free == NULL) {
-        real_free = (void (*) (void *)) dlsym(RTLD_NEXT, "free");
-    }
-
-    if (no_hook) {
-        real_free(ptr);
-        return;
-    }
-
-    no_hook = 1;
-    erim_free(ptr);
-    no_hook = 0;
-}
-#endif
-
-#ifdef REALLOC
-void * realloc(void * ptr, size_t size) {
-    void *ret;
-
-    if (real_realloc == NULL) {
-        real_realloc = (void *(*) (void *, size_t)) dlsym(RTLD_NEXT, "realloc");
-    }
-
-    if (no_hook) {
-        return (*real_realloc)(ptr, size);
-    }
-
-    no_hook = 1;
-    ret = (*erim_realloc)(ptr, size);
-    no_hook = 0;
-
-    return ret;
-}
-#endif
-
-#ifdef MMAP
-void * mmap(void * addr, size_t length, int prot, int flags, int fd, off_t offset) {
-    void *ret;
-
-    if (real_mmap == NULL) {
-        real_mmap = (void *(*) (void *, size_t, int, int, int, off_t)) dlsym(RTLD_NEXT, "mmap");
-    }
-
-    if (no_hook) {
-        return (*real_mmap)(addr, length, prot, flags, fd, offset);
-    }
-
-    no_hook = 1;
-    ret = erim_mmap_domain(addr, length, prot, flags, fd, offset, ERIM_EXEC_DOMAIN(__rdpkru()));
-    no_hook = 0;
-
-    return ret;
-}
-#endif
-
-#ifdef MUNMAP
-int munmap(void * addr, size_t length) {
-    int ret;
-
-    if (real_munmap == NULL) {
-        real_munmap = (int (*) (void *, size_t)) dlsym(RTLD_NEXT, "munmap");
-    }
-
-    if (no_hook) {
-        return real_munmap(addr, length);
-    }
-
-    no_hook = 1;
-    ret = erim_munmap(addr, length);
-    no_hook = 0;
-
-    return ret;
-}
-#endif
-
 /* Library loading */
-
 #ifdef DLOPEN
 void * dlopen(const char * input, int flag) {
     if (real_dlopen == NULL) {
@@ -183,10 +89,10 @@ void * dlopen(const char * input, int flag) {
 
     void *handle = real_dlopen(pathname, flag | RTLD_GLOBAL);
 
-    logMessage("Saving library addresses and sizes...", verbose);
+    PRELOAD_DBM("Saving library addresses and sizes...");
     getMemoryRegions(&appMap, id, pathname);
 
-    printAppMap(appMap, verbose);
+    printAppMap(appMap, 1);
 
     remove(input);
     return handle;
@@ -194,7 +100,6 @@ void * dlopen(const char * input, int flag) {
 #endif
 
 /* Threads */
-
 #ifdef PTHREAD_CREATE
 int pthread_create(pthread_t * thread, const pthread_attr_t * attr, void * ( * start_routine)(void * ), void * arg) {
     if (real_pthread_create == NULL) {
@@ -205,7 +110,7 @@ int pthread_create(pthread_t * thread, const pthread_attr_t * attr, void * ( * s
 
     if (result == 0) {
         int domain = ERIM_EXEC_DOMAIN(__rdpkru());
-        logMessage("New thread in map", verbose);
+        PRELOAD_DBM("New thread running on domain %d in map", domain);
         insertThread(&threadMap, domain, *thread);
     }
 
@@ -221,7 +126,7 @@ void pthread_exit(void* value_ptr) {
     
     pthread_t currentThread = pthread_self();
     int domain = ERIM_EXEC_DOMAIN(__rdpkru());
-    logMessage("Removing thread from map", verbose);
+    PRELOAD_DBM("Removing thread running on domain %d from map", domain);
     removeThread(&threadMap, domain, currentThread);
 
     real_pthread_exit(value_ptr);
--- mpk/ld-preload/preload.h
+++ mpk/ld-preload/preload.h
@@ -1,18 +1,13 @@
 #include "utils/appmap.h"
 #include "utils/threadmap.h"
 #include "helpers/helpers.h"
-#include "../erim/common.h"
-#include "../erim/erim.h"
+#include <common.h>
+#include <erim.h>
 
-#define MALLOC
-#define REALLOC
-#define FREE
-#define MMAP
-#define MUNMAP
 #define DLOPEN
 #define PTHREAD_CREATE
 #define PTHREAD_EXIT
 
 void setAppPermissions(const char* id, int protectionFlag, int pkey);
 int isEmpty(int domain);
-
+int findEmptyDomain();
