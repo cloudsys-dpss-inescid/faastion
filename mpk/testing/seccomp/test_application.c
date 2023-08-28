c94df7ae470102ba45a923d78870867c522ed1fe
ist192549 <ist192549@tecnico.ulisboa.pt>
2023-08-28 15:58:10 +0100

    Added a seccomp test.

---
+++ mpk/testing/seccomp/test_application.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+int main() {
+    size_t size = sizeof(int);
+
+    void *mapped_mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, -1, 0);
+    if (mapped_mem == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    if (munmap(mapped_mem, size) == -1) {
+        perror("munmap");
+        return 1;
+    }
+
+    return 0;
+}
