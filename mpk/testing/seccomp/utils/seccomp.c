7ff33346faa0ebf0bb91922ddcf80d7336598f39
cidra <cidra@intel14v2.gsd.inesc-id.pt>
2023-08-29 17:57:18 +0100

    Updated seccomp module.

--- mpk/testing/seccomp/utils/seccomp.c
+++ mpk/testing/seccomp/utils/seccomp.c
@@ -16,16 +16,16 @@
 #include <linux/filter.h>
 #include <linux/audit.h>
 #include "seccomp.h"
-//#include <seccomp.h>
 
 #define MODE_STRICT_STRING "--mode-strict"
 #define MODE_FS_STRING "--mode-fs" 
 #define MODE_NET_STRING "--mode-net"
 #define MODE_ALLOW_RW_STRING "--mode-allow-rw"
+#define MODE_MPK_STRING "--mode-mpk"
 
 #define FILE_SIZE 256
 
-typedef enum seccomp_mode_enum { MODE_STRICT, MODE_ALLOW_RW } seccomp_mode;
+typedef enum seccomp_mode_enum { MODE_STRICT, MODE_ALLOW_RW, MODE_MPK } seccomp_mode;
 
 int target_pid;
 volatile int nfd = 0;
@@ -36,6 +36,85 @@ char **args;
 int mode;
 
 unsigned long intercept_count = 0;
+
+struct sock_filter mpk_filter[] = {
+    BPF_STMT(BPF_LD + BPF_W + BPF_ABS, (offsetof(struct seccomp_data, arch))),
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, AUDIT_ARCH_X86_64, 1, 0),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_STMT(BPF_LD + BPF_W + BPF_ABS, (offsetof(struct seccomp_data, nr))),
+
+    // pkeys
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_pkey_mprotect, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_pkey_alloc, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_pkey_free, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    // privileges
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setuid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setgid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setpgid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+    
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setsid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setreuid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setregid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setresuid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setresgid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setresgid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setfsgid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_setfsuid, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_chmod, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_fchmod, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_fchmodat, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_chown, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_fchown, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_fchownat, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_lchown, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    // exit
+    BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_exit, 0, 1),
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),
+
+    BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW),
+};
 
 struct sock_filter strict_filter[] = {
     BPF_STMT(BPF_LD + BPF_W + BPF_ABS, (offsetof(struct seccomp_data, arch))),
@@ -126,6 +205,8 @@ int get_seccomp_mode(char *mode) {
         return MODE_STRICT;
     else if (strncmp(mode, MODE_ALLOW_RW_STRING, sizeof(MODE_ALLOW_RW_STRING)) == 0)
         return MODE_ALLOW_RW;
+    else if (strncmp(mode, MODE_MPK_STRING, sizeof(MODE_MPK_STRING)) == 0)
+        return MODE_MPK;
     else
         return -1;
 }
@@ -175,6 +256,10 @@ int install_notify_filter(int mode) {
             prog.len = (unsigned short)(sizeof(allow_rw_filter) / sizeof(allow_rw_filter[0]));
             prog.filter = allow_rw_filter;
             break;
+        case MODE_MPK:
+            prog.len = (unsigned short)(sizeof(mpk_filter) / sizeof(mpk_filter[0]));
+            prog.filter = mpk_filter;
+            break;
         default:
             break;
     }
@@ -212,6 +297,29 @@ void sigterm_handler(int signum) {
     exit(0);
 }
 
+void handle_mmap(struct seccomp_notif *req) {
+    SECC_DBM("Intercepted mmap syscall!");
+    //protect memory previously
+    // int domain = ERIM_EXEC_DOMAIN(__rdpkru());
+    // if (pkey_mprotect((void *)req->data.args[0], (size_t)req->data.args[1], (int)req->data.args[2], domain) == -1) {
+    //     perror("pkey_mprotect");
+    // }
+}
+
+void handle_clone(struct seccomp_notif *req) {
+    SECC_DBM("Intercepted clone syscall!");
+    // int domain = ERIM_EXEC_DOMAIN(__rdpkru());
+    // insertThread(&threadMap, domain);
+}
+
+void allow_syscall(int nfd, struct seccomp_notif_resp *resp, unsigned long long id) {
+    resp->id = id;
+    resp->flags = SECCOMP_USER_NOTIF_FLAG_CONTINUE;
+    if (ioctl(nfd, SECCOMP_IOCTL_NOTIF_SEND, resp) == -1) {
+        perror("ioctl(SECCOMP_IOCTL_NOTIF_SEND)");
+    }
+}
+
 void handle_notifications(int nfd) {
     struct seccomp_notif_sizes sizes;
     if (syscall(SYS_seccomp, SECCOMP_GET_NOTIF_SIZES, 0, &sizes) < 0) {
@@ -233,22 +341,25 @@ void handle_notifications(int nfd) {
 
         // Print notification
         intercept_count++;
-#ifdef DEBUG
-        fprintf(stderr, "Received notifaction id [%lld], from tid: %d, syscall nr: %d\n", req->id, req->pid, req->data.nr);
-#endif
-
+        SECC_DBM("Received notifaction id [%lld], from tid: %d, syscall nr: %d", req->id, req->pid, req->data.nr);
+        
         // Validate notification
         if (ioctl(nfd, SECCOMP_IOCTL_NOTIF_ID_VALID, &req->id) == -1 ) {
             perror("ioctl(SECCOMP_IOCTL_NOTIF_ID_VALID)");
             continue;
         }
 
-        // Send response
-        resp->id = req->id;
-        resp->flags = SECCOMP_USER_NOTIF_FLAG_CONTINUE;
-        if (ioctl(nfd, SECCOMP_IOCTL_NOTIF_SEND, resp) == -1) {
-            perror("ioctl(SECCOMP_IOCTL_NOTIF_SEND)");
-            continue;
-        }
+        // Handle specific syscalls
+        switch(req->data.nr) {
+            case __NR_mmap:
+                handle_mmap(req);
+                break;
+            case __NR_clone:
+                handle_clone(req);
+                break;
+            default:
+                break;
+        }        
+        allow_syscall(nfd, resp, req->id);
     }
 }
--- mpk/testing/seccomp/utils/seccomp.h
+++ mpk/testing/seccomp/utils/seccomp.h
@@ -3,6 +3,19 @@
 
 #include <unistd.h>
 
+/*
+ * Debug prints
+ */
+#ifdef SECC_DBG
+  #define SECC_DBM(...)				\
+    do {					\
+      fprintf(stderr, __VA_ARGS__);		\
+      fprintf(stderr, "\n");			\
+    } while(0)
+#else // disable debug
+  #define SECC_DBG(...)
+#endif
+
 char *get_pathname(char name[]);
 int exec(char pathname[], char *argv[], int mode);
 int get_seccomp_mode(char *mode);
