a837b3b1ad7a2886128624d23ce57762cc63cc92
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2024-10-10 17:38:50 +0100

    Create prototype for faastlane mode

--- graalvisor/src/main/java/org/graalvm/argo/graalvisor/RuntimeProxy.java
+++ graalvisor/src/main/java/org/graalvm/argo/graalvisor/RuntimeProxy.java
@@ -84,7 +84,13 @@ public abstract class RuntimeProxy {
         server.createContext("/warmup", new WarmupHandler());
         server.createContext("/register", new RegisterHandler());
         server.createContext("/deregister", new DeregisterHandler());
-        server.setExecutor(Executors.newCachedThreadPool());
+
+        String faastlane_mode = System.getenv("faastlane");
+        if (faastlane_mode != null && faastlane_mode.equals("true")) {
+            server.setExecutor(Executors.newFixedThreadPool(15));
+        } else {
+            server.setExecutor(Executors.newCachedThreadPool());
+        }
     }
 
     static {
