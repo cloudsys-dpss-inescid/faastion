a9f9939d53f8a1281d160c15805dd5d2549cc872
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2024-10-11 20:28:17 +0100

    Create filehashing benchmark build script

--- benchmarks/src/java/gv-file-hashing/build_script.sh
+++ benchmarks/src/java/gv-file-hashing/build_script.sh
@@ -9,35 +9,22 @@ JAVA_AGENT="$JAVASSIST_HOME/target/JavassistWrapper-1.0-jar-with-dependencies.ja
 JNI_INCLUDE="-I$DEF_JAVA_HOME/include -I$DEF_JAVA_HOME/include/linux"
 ERIM_INCLUDE="-I$ERIM_HOME/src/erim -I$ERIM_HOME/src/common"
 
-CFLAGS="-Wall -g -fPIC -shared $JNI_INCLUDE -I/usr/include/"
+CFLAGS="-Wall -g -fPIC -shared $JNI_INCLUDE"
 CFLAGS_PROC="-Wall -g -fPIC $JNI_INCLUDE"
-SFLAGS="$CFLAGS -O0 -fno-inline $ERIM_INCLUDE -I$GRAALVISOR_HOME/src/main/c/memisolation/src"
-SFLAGS_PROC="$CFLAGS_PROC -O0 -fno-inline $ERIM_INCLUDE -I$GRAALVISOR_HOME/src/main/c/memisolation/src"
+SFLAGS="$CFLAGS -O0 -fno-inline -I$GRAALVISOR_HOME/src/main/c/pkru-sandbox/src"
 
 BENCHMARK_NAME="filehashing"
 SNIPPETS_DIR="$DIR/build/snippets"
 
-function run_hotspot {
-	rm -rf config-dir
-	CLASS_PATH="classes/java/main"
-
-	cd build
-	export LD_LIBRARY_PATH=$GRAALVISOR_HOME/build/libs:libs:$LD_LIBRARY_PATH
-	$JAVA_HOME/bin/java \
-			-Djava.awt.headless=true \
-			-agentlib:native-image-agent=config-output-dir=config-dir/ \
-			-cp $CLASS_PATH:libs/filehashing-1.0-all.jar \
-			-Djava.library.path=$LD_LIBRARY_PATH \
-			com.jni.FileHashing
-}
+CURRENT_LIBRARY_PATH=$LD_LIBRARY_PATH
 
 function build_ni {
-	CLASS_PATH="classes/java/main"
-
 	cd build
-	export LD_LIBRARY_PATH=$GRAALVISOR_HOME/build/libs:libs:$LD_LIBRARY_PATH
+
+	export LD_LIBRARY_PATH=$GRAALVISOR_HOME/build/libs:libs:$CURRENT_LIBRARY_PATH
 	$JAVA_HOME/bin/native-image \
 			--no-fallback \
+			--enable-url-protocols=http \
 			-cp $CLASS_PATH:libs/filehashing-1.0-all.jar:$ARGO_HOME/graalvisor-lib/build/libs/graalvisor-lib-1.0-guest.jar \
 			-DGraalVisorGuest=true \
 			-Djava.library.path=$LD_LIBRARY_PATH \
@@ -46,16 +33,23 @@ function build_ni {
 			-H:ConfigurationFileDirectories=../ni-agent-config \
 			-H:+ReportExceptionStackTraces \
 			$NI_BIN_OPTS \
-			-H:Name=lib$BENCHMARK_NAME
+			-H:Name=lib$FUNCTION_ID
+
+	cd -
 }
 
-function build_ni_standalone {
-	NI_BIN_OPTS="com.jni.FileHashing"
+function build_faastion_image {
+	NI_BIN_OPTS="--shared"
+	CLASS_PATH="$DIR/output"
+
 	build_ni
 }
 
-function build_ni_sharedlibrary {
+function build_vanila_image {
 	NI_BIN_OPTS="--shared"
+	CLASS_PATH="$DIR/java/main"
+	FUNCTION_ID="$BENCHMARK_NAME"
+
 	build_ni
 }
 
@@ -64,31 +58,33 @@ function build_java_agent {
 }
 
 function build_native_library {
-	musl-gcc -static $CFLAGS -o $GRAALVISOR_HOME/build/libs/lib$BENCHMARK_NAME-jni.so $DIR/src/main/c/FileHashing.c
+	gcc --shared -fpic $CFLAGS -o $GRAALVISOR_HOME/build/libs/lib$BENCHMARK_NAME-jni.so $DIR/src/main/c/FileHashing.c
 }
 
 function build_snippets {
-	export LD_LIBRARY_PATH=$GRAALVISOR_HOME/build/libs:libs:$LD_LIBRARY_PATH
-	for file in "$SNIPPETS_DIR"/*.c; do
-		name=$(basename "$file" .c)
-		gcc $SFLAGS_PROC -o $GRAALVISOR_HOME/build/libs/$name-proc $file -L$GRAALVISOR_HOME/build/libs -lmemiso -Wl,-rpath,$GRAALVISOR_HOME/build/libs
-		gcc $SFLAGS -o $GRAALVISOR_HOME/build/libs/lib$name.so $file -L$GRAALVISOR_HOME/build/libs -lmemiso
-	done
+	pathname=$(ls "$SNIPPETS_DIR"/*.c)
+	file=${pathname##*/}
+	name=${file%.*}
+	gcc $SFLAGS -DREMOVE_NNS_LIMIT -o $GRAALVISOR_HOME/build/libs/lib${FUNCTION_ID}-${name}.so $pathname -L$GRAALVISOR_HOME/build/libs -lpkru
 }
 
 function manipulate_bytecode {
 	CLASS_PATH="build/classes/java/main"
 	ENTRYPOINT="com.jni.FileHashing"
-	TOOL="NativeRedirection"
+	TOOL="JNITemplateBuilder"
+
+	rm -f $GRAALVISOR_HOME/build/libs/lib${FUNCTION_ID}-hash.so
 	
 	mkdir -p $DIR/build/snippets
 	
 	export BENCHMARK_NAME="$BENCHMARK_NAME"
 	export SNIPPETS_DIR="$SNIPPETS_DIR"
+	export FUNCTION_ID="$FUNCTION_ID"
 	export ENV="memisolation"
+
 	$DEF_JAVA_HOME/bin/java \
 			-cp $CLASS_PATH \
-			-javaagent:$JAVA_AGENT=$TOOL::$CLASS_PATH \
+			-javaagent:$JAVA_AGENT=$TOOL::output \
 			$ENTRYPOINT
 }
 
@@ -123,21 +119,15 @@ cd $DIR &> /dev/null
 # Build application.
 ./gradlew clean shadowJar assemble
 
-# Build native lib.
 build_native_library
 
-# Manipulate app's bytecode.
-manipulate_bytecode
+build_vanila_image
 
-# Build generated snippets.
-build_snippets
-
-TARGET=$1
-if [ ! -z "$TARGET" ]
-then
-	$TARGET
-exit 0
-else
-	build_ni_sharedlibrary
-	exit 0
-fi
+CONCURRENCY_LEVEL=32
+for i in $(seq 1 $CONCURRENCY_LEVEL); do
+	FUNCTION_ID="$BENCHMARK_NAME${i}"
+	manipulate_bytecode
+	build_snippets
+	build_faastion_image
+done
+exit
