be6bba983a22948d6243f7b13f9e76f54f56f1ff
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2024-10-21 16:43:35 +0100

    Add new benchmarks to build to script

--- benchmarks/scripts/metrics/build_benchmarks.sh
+++ benchmarks/scripts/metrics/build_benchmarks.sh
@@ -23,15 +23,23 @@ function gv_aes_encryption {
         BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-aes-encryption"
 }
 
+function gv_hello_world {
+	BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-hello-world"
+}
+
 if [ -z $CONCURRENCY_LEVEL ]; then
 	export CONCURRENCY_LEVEL=32
 fi
 
-benchmarks=(gv_native_factors gv_filehashing)
-build_duration=$(echo "scale=2; ${#benchmarks[@]} * 30 / 60" | bc)
+native_benchmarks=(gv_native_hw gv_native_factors gv_filehashing)
+managed_benchmarks=(gv_hello_world gv_aes_encryption)
+
+build_duration=$(echo "scale=2; ${#native_benchmarks[@]} * 40 / 60" | bc)
 total_duration=$(echo "scale=2; ($CONCURRENCY_LEVEL + 1) * $build_duration" | bc)
+total_duration=$(echo "scale=2; $total_duration + (${#managed_benchmarks[@]} * 40 / 60)" | bc)
 echo "Estimated build time ~= $total_duration mins"
-for benchmark_name in ${benchmarks[@]}
+
+for benchmark_name in ${native_benchmarks[@]}
 do
 	$benchmark_name
 	cd "$BENCHMARK_DIR"
@@ -39,7 +47,10 @@ do
 	cd -
 done
 
-gv_aes_encryption
-cd "$BENCHMARK_DIR"
-./build_script.sh
-cd -
+for benchmark_name in ${managed_benchmarks[@]}
+do
+	$benchmark_name
+	cd "$BENCHMARK_DIR"
+	./build_script.sh
+	cd -
+done
