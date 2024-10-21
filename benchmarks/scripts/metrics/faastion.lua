7a59bce3630c75f6cf299eacc9592d2de10eea3a
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2024-10-21 16:36:39 +0100

    Add response function to lua script

--- benchmarks/scripts/metrics/faastion.lua
+++ benchmarks/scripts/metrics/faastion.lua
@@ -16,3 +16,9 @@ request = function()
     
     return wrk.format("POST", nil, nil, wrk.body)
 end
+
+-- function response(status, headers, body)
+--    print(body)
+-- end
+
+-- wrk.response = response
