8dde9e099992a9fcadcb161b0e38b98ab144b905
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2024-10-21 04:11:03 +0100

    Update build scripts to generate native binary

--- benchmarks/src/java/gv-native-factorization/build_script.sh
+++ benchmarks/src/java/gv-native-factorization/build_script.sh
@@ -18,6 +18,23 @@ SNIPPETS_DIR="$DIR/build/snippets"
 
 CURRENT_LIBRARY_PATH=$LD_LIBRARY_PATH
 
+function build_native_binary {
+	NI_BIN_OPTS="com.jni.Factorization"
+	cd build
+
+	export LD_LIBRARY_PATH=$GRAALVISOR_HOME/build/libs:libs:$CURRENT_LIBRARY_PATH
+	$JAVA_HOME/bin/native-image \
+			--no-fallback \
+			-cp $CLASS_PATH:libs/factors-1.0-all.jar \
+			-Djava.library.path=$LD_LIBRARY_PATH \
+			-H:ConfigurationFileDirectories=../ni-agent-config \
+			-H:+ReportExceptionStackTraces \
+			$NI_BIN_OPTS \
+			-H:Name=$GRAALVISOR_HOME/build/libs/$BENCHMARK_NAME-proc
+
+	cd -
+}
+
 function build_ni {
 	cd build
 
@@ -118,6 +135,8 @@ cd $DIR &> /dev/null
 # Build application.
 ./gradlew clean shadowJar assemble
 
+build_native_binary
+
 build_native_library
 
 build_vanila_image
@@ -132,4 +151,4 @@ for i in $(seq 1 $CONCURRENCY_LEVEL); do
 	build_snippets
 	build_faastion_image
 done
-exit 
+exit
--- benchmarks/src/java/gv-native-hw/build_script.sh
+++ benchmarks/src/java/gv-native-hw/build_script.sh
@@ -20,6 +20,23 @@ SNIPPETS_DIR="$DIR/build/snippets"
 
 CURRENT_LIBRARY_PATH=$LD_LIBRARY_PATH
 
+function build_native_binary {
+	NI_BIN_OPTS="com.jni.Factorization"
+	cd build
+
+	export LD_LIBRARY_PATH=$GRAALVISOR_HOME/build/libs:libs:$CURRENT_LIBRARY_PATH
+	$JAVA_HOME/bin/native-image \
+			--no-fallback \
+			-cp $CLASS_PATH:libs/factors-1.0-all.jar \
+			-Djava.library.path=$LD_LIBRARY_PATH \
+			-H:ConfigurationFileDirectories=../ni-agent-config \
+			-H:+ReportExceptionStackTraces \
+			$NI_BIN_OPTS \
+			-H:Name=$GRAALVISOR_HOME/build/libs/$BENCHMARK_NAME-proc
+
+	cd -
+}
+
 function build_ni {
 	cd build
 
@@ -128,6 +145,8 @@ cd $DIR &> /dev/null
 
 # Build application.
 ./gradlew clean shadowJar assemble
+
+build_native_binary
 
 build_native_library
 
