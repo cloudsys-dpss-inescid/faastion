578b90cef2f54963faef7c8bfee0560f6c17e996
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2024-10-22 01:38:07 +0100

    Add benchmarks to build script

--- benchmarks/scripts/metrics/build_benchmarks.sh
+++ benchmarks/scripts/metrics/build_benchmarks.sh
@@ -27,16 +27,32 @@ function gv_hello_world {
 	BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-hello-world"
 }
 
+function gv_httpequest {
+	BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-httprequest"
+}
+
+function gv_native_matmul {
+	BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-native-matmul"
+}
+
+function gv_matrixmul {
+	BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-matrixmul"
+}
+
+function gv_factorization {
+	BENCHMARK_DIR="$BENCHMARKS_HOME/src/java/gv-factorization"
+}
+
 if [ -z $CONCURRENCY_LEVEL ]; then
 	export CONCURRENCY_LEVEL=32
 fi
 
-native_benchmarks=(gv_native_hw gv_native_factors gv_filehashing)
-managed_benchmarks=(gv_hello_world gv_aes_encryption)
+native_benchmarks=(gv_native_hw gv_native_factors gv_filehashing gv_native_matmul)
+managed_benchmarks=(gv_hello_world gv_aes_encryption gv_httprequest gv_matrixmul gv_factorization)
 
 build_duration=$(echo "scale=2; ${#native_benchmarks[@]} * 40 / 60" | bc)
-total_duration=$(echo "scale=2; ($CONCURRENCY_LEVEL + 1) * $build_duration" | bc)
-total_duration=$(echo "scale=2; $total_duration + (${#managed_benchmarks[@]} * 40 / 60)" | bc)
+total_duration=$(echo "scale=2; ($CONCURRENCY_LEVEL + 2) * $build_duration" | bc)
+total_duration=$(echo "scale=2; $total_duration + (${#managed_benchmarks[@]} * 40 * 2 / 60)" | bc)
 echo "Estimated build time ~= $total_duration mins"
 
 for benchmark_name in ${native_benchmarks[@]}
