bc7d9e02a4564ef123be1873e5c7f0c19cbdddd7
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-03-21 18:27:11 +0000

    Create native bfs benchmark

---
+++ benchmarks/src/java/gv-native-bfs/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+HOST=localhost
+PORT=8080
+#NAME=bfs
+
+NAME=$1
+
+function println {
+	echo -n -e '\n'
+}
+
+curl -s -X POST $HOST:$PORT/register?name=$NAME\&entryPoint=com.jni.BFS\&language=java \
+	-H 'Content-Type: application/json' --data-binary @"${DIR}/build/lib${NAME}.so" \
+&& println \
+&& curl -s -X POST $HOST:$PORT -H 'Content-Type: application/json' \
+	--data-binary '{"name":"'${NAME}'","async":"false","arguments":"{}"}' \
+&& println
---
+++ benchmarks/src/java/gv-native-bfs/src/main/c/BFS.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <igraph.h>
+
+#include "com_jni_BFS.h"
+
+#define EDGES       10
+
+#ifdef INPUT_TEST
+#define INPUT 10
+#elif defined(INPUT_SMALL)
+#define INPUT 10000
+#else
+#define INPUT 100000
+#endif /* INPUT_TEST */
+
+JNIEXPORT void JNICALL Java_com_jni_BFS_bfs(JNIEnv *env, jobject obj) {
+        igraph_t graph;
+        igraph_rng_seed(igraph_rng_default(), 42);
+        igraph_barabasi_game(&graph, INPUT, 1, EDGES, NULL, 1, 0, IGRAPH_DIRECTED, IGRAPH_BARABASI_PSUMTREE, NULL);
+
+        igraph_bfs(&graph, 0, NULL, IGRAPH_OUT,
+                1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+        igraph_destroy(&graph);
+}
