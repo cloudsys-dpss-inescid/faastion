9c0d1dd837181741253bc4a76a0feede00165adf
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-08-22 21:07:47 +0100

    Adapt start script to support faastion_lpi and faastlane

--- images/build_container_image.sh
+++ images/build_container_image.sh
@@ -15,13 +15,18 @@ echo '#!/bin/bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
+if [ "$1" = "--enable-lpi" ]; then
+    export LPI=true
+elif [ "$1" = "--enable-early-booking" ]; then
+    export faastlane=true
+fi
+
 library_path="'$BUILD_HOME':'$LIBC_HOME'/lib:/lib/x86_64-linux-gnu"
 env GLIBC_TUNABLES="glibc.rtld.nns=16" LD_LIBRARY_PATH="$library_path" LD_PRELOAD="libmem.so" $DIR/build/native-image/polyglot-proxy
 ' > $DISK/graalvisor/start.sh
 
 # Copy graalvisor and init.
 cp $BUILD_HOME/*.so $DISK/$BUILD_HOME/.
-cp $BUILD_HOME/*-proc $DISK/$BUILD_HOME/. # required by faastion LPI
 cp -r $LIBC_HOME/* $DISK/$LIBC_HOME/.
 cp /usr/lib/libigraph.so.3 $DISK/$BUILD_HOME/. # required by search algorithms
 cp $GRAALVISOR_BINARY $DISK/graalvisor/build/native-image/polyglot-proxy
