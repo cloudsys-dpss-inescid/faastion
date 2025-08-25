bcb1d50fea6245a17d87e292051a6c4af84804d4
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-08-25 16:50:34 +0100

    Reduce workload size

--- benchmarks/src/java/graalvisor-runtime/gv-native-mst/src/main/c/MST.c
+++ benchmarks/src/java/graalvisor-runtime/gv-native-mst/src/main/c/MST.c
@@ -16,7 +16,7 @@
 JNIEXPORT void JNICALL Java_com_jni_MST_mst(JNIEnv *env, jobject obj) {
         igraph_t graph;
         igraph_rng_seed(igraph_rng_default(), 42);
-        igraph_barabasi_game(&graph, INPUT, 1, EDGES, NULL, 1, 0, IGRAPH_DIRECTED, IGRAPH_BARABASI_PSUMTREE, NULL);
+        igraph_barabasi_game(&graph, 100, 1, EDGES, NULL, 1, 0, IGRAPH_DIRECTED, IGRAPH_BARABASI_PSUMTREE, NULL);
 
 	igraph_vector_int_t edges;
         igraph_vector_int_init(&edges, 0);
