7094dcc9e313b158a066b3f0d780a99ec23ee7d6
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-09-20 18:46:20 +0100

    Implement thumbnailer using opencv library

--- benchmarks/src/java/graalvisor-runtime/gv-thumbnail/src/main/java/com/thumbnail/Thumbnail.java
+++ benchmarks/src/java/graalvisor-runtime/gv-thumbnail/src/main/java/com/thumbnail/Thumbnail.java
@@ -1,63 +1,80 @@
 package com.thumbnail;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
 import java.util.HashMap;
 
-import java.util.Random;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.Core;
+import org.opencv.core.Size;
+import nu.pattern.OpenCV;
 
-import java.io.File;
-import java.awt.Image;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Thumbnail {
+    
+    public static String IMG_FILENAME = String.format("img-%d.png", ThreadLocalRandom.current().nextInt(0, 1024 + 1));
 
-    public static BufferedImage scale(BufferedImage source, double ratio) {
-        if (source == null) {
-            return null;
-        }
-        int width = (int)(source.getWidth() * ratio);
-        int height = (int)(source.getHeight() * ratio);
-        int imageType = BufferedImage.TYPE_INT_ARGB;
-        BufferedImage bi = new BufferedImage(width, height, imageType);
-        Image result = source.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        bi.createGraphics().drawImage(result, 0, 0, null);
-        return bi;
-    }
-
-    public static boolean resize(String url, double ratio) throws IOException {
-        BufferedImage img = ImageIO.read(new URL(url));
-        BufferedImage bimg = scale(img, ratio);
-        String filePath = "/tmp/img-"
-                + new Random().ints(8, 0, 11)
-                    .mapToObj(n -> String.valueOf(n))
-                    .collect(Collectors.joining())
-                + ".png";
-        ImageIO.write(bimg, "png", new File(filePath));
+    public static boolean resize(String filePath, double ratio, String outFile) {
+        nu.pattern.OpenCV.loadLocally();
+		Mat image = Imgcodecs.imread(filePath);
+		Imgproc.resize(image, image, new Size(0, 0), ratio, ratio);
+		Imgcodecs.imwrite(outFile, image);
         return true;
     }
 
+    public static byte[] fromInputStream(InputStream is) throws Exception {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[16384];
+
+        while ((nRead = is.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        return buffer.toByteArray();
+    }
+
+    public static byte[] downloadBytes(String url) {
+        try {
+            URLConnection conn = new URL(url).openConnection();
+            InputStream is = conn.getInputStream();
+            byte[] bytes = fromInputStream(is); 
+            is.close();
+            return bytes;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static HashMap<String, Object> main(Map<String, Object> input) {
-        HashMap<String, Object> output = new HashMap<>();
+        String tmpDir = (String) input.get("tmpDir");
         String url = "http://127.0.0.1:8000/snap.png";
-        double ratio = 0.25;
+        String filePath = tmpDir + "/" + IMG_FILENAME;
+        String outFile = tmpDir + "/output.png"; 
+        HashMap<String, Object> output = new HashMap<>();
         boolean success = false;
         try {
-            success = resize(url, ratio);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            FileOutputStream stream = new FileOutputStream(filePath);
+            stream.write(downloadBytes(url));
+            success = resize(filePath, 0.25, outFile);
+        } catch (Exception e) {}
         output.put("success", success);
         return output;
     }
 
     public static void main(String[] args) {
         HashMap<String, Object> output = new HashMap<>();
+        output.put("tmpDir", "/tmp/sandbox-0");
         output = main(output);
         System.out.println(output);
     }
