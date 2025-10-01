cc1b0f0a96c2054c431375dc5ed5782d17621455
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-10-01 22:02:27 +0100

    Add faastlane mockup mode

--- graalvisor/src/main/java/org/graalvm/argo/graalvisor/SubstrateVMProxy.java
+++ graalvisor/src/main/java/org/graalvm/argo/graalvisor/SubstrateVMProxy.java
@@ -172,9 +172,17 @@ public class SubstrateVMProxy extends RuntimeProxy {
 
         private AtomicBoolean open = new AtomicBoolean(true);
 
+        private int maxFaastlaneWorkers;
+    
+        private boolean faastlane;
+
         public FunctionPipeline(PolyglotFunction function) {
             this.function = function;
             this.queue = new ConcurrentLinkedQueue<>();
+            String faastlane_mode = System.getenv("faastlane");
+            this.faastlane = faastlane_mode != null && faastlane_mode.equals("true");
+            String faastlane_workers = System.getenv("faastlane_workers");
+            this.maxFaastlaneWorkers = faastlane_workers == null ? 15 : Integer.parseInt(faastlane_workers);
         }
 
         public void invokeInCachedSandbox(Request req) {
@@ -185,10 +193,12 @@ public class SubstrateVMProxy extends RuntimeProxy {
             // Checks if we need to launch additional sandboxes.
             if (cWorkers < cActive && (cMaxWorkers == 0 || cWorkers < cMaxWorkers)) {
                 synchronized (this) {
-                    // Repeats the check to verify that we won the race.
-                    if (workers.intValue() < active.intValue()) {
-                        workers.incrementAndGet();
-                        new Worker(this).start();
+                    if (!faastlane || workers.intValue() < maxFaastlaneWorkers) {
+                        // Repeats the check to verify that we won the race.
+                        if (workers.intValue() < active.intValue()) {
+                            workers.incrementAndGet();
+                            new Worker(this).start();
+                        }
                     }
                 }
             }
