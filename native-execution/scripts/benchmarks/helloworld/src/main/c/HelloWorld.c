4ff5c5cacb895b9f00949b293a25e67227c1e039
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-10-09 11:37:10 +0100

    Cleanup unnecessary data

--- native-execution/scripts/benchmarks/helloworld/src/main/c/HelloWorld.c
+++
@@ -1,6 +0,0 @@
-#include "../../../build/generated/sources/headers/java/main/HelloWorld.h"
-#include <stdio.h>
-
-JNIEXPORT void JNICALL Java_HelloWorld_helloworld(JNIEnv *env, jobject obj) {
-    printf("Hello World!");
-}
