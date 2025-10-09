04ff007c20def7640c0b5730146782952560c2d1
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-10-09 16:12:03 +0100

    Update Dockerfile

--- images/faastion/build_container_image.sh
+++ images/faastion/build_container_image.sh
@@ -3,7 +3,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 GRAALVISOR_HOME=$ARGO_HOME/graalvisor
-BUILD_HOME=$GRAALVISOR_HOME/build/libs/
+LIB_DIR=$GRAALVISOR_HOME/shared/
 GRAALVISOR_BINARY=$GRAALVISOR_HOME/build/native-image/polyglot-proxy
 
 if [ -z "$ARGO_HOME" ]; then
@@ -14,7 +14,8 @@ fi
 # Prepare directory used to setup the filesystem.
 DISK=$DIR/disk
 rm -rf $DISK &> /dev/null
-mkdir -p $DISK/graalvisor/build/native-image $DISK/$BUILD_HOME $DISK/$LIBC_HOME
+mkdir -p $DISK/
+# mkdir -p $DISK/graalvisor/build/native-image $DISK/$LIB_DIR
 
 echo '#!/bin/bash
 
@@ -28,15 +29,22 @@ fi
 
 library_path="'$BUILD_HOME':'$LIBC_HOME'/lib:/usr/local/lib:/lib/x86_64-linux-gnu"
 env GLIBC_TUNABLES="glibc.rtld.nns=16" LD_LIBRARY_PATH="$library_path" LD_PRELOAD="libmem.so" $DIR/build/native-image/polyglot-proxy
-' > $DISK/graalvisor/start.sh
+' > $DISK/start.sh
+
+cd $ARGO_HOME
+git archive -o faastion.zip --prefix="faastion/" HEAD
+cd - &> /dev/null
 
 # Copy graalvisor and init.
-cp $BUILD_HOME/*.so $DISK/$BUILD_HOME/.
-cp -r $LIBC_HOME/* $DISK/$LIBC_HOME/.
-cp $GRAALVISOR_BINARY $DISK/graalvisor/build/native-image/polyglot-proxy
+cp $DIR/glibc-2.35.patch $DISK/.
+cp $DIR/glibc-2.35.tar.gz $DISK/.
+mv $ARGO_HOME/faastion.zip $DISK/.
+# cp $BUILD_HOME/*.so $DISK/$BUILD_HOME/.
+# cp -r $LIBC_HOME/* $DISK/$LIBC_HOME/.
+# cp $GRAALVISOR_BINARY $DISK/graalvisor/build/native-image/polyglot-proxy
 
 # Build docker.
-docker build --build-arg libc_home=$LIBC_HOME -t faastion $DIR
+docker build -t faastion $DIR
 
 # Remove directory used to create the image.
 rm -rf $DISK &> /dev/null
