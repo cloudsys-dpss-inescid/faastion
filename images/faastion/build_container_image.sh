442917cee3c6f6518e3078a948839dbf562c5e80
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-10-10 15:58:18 +0100

    Update start script for faastion container image

--- images/faastion/build_container_image.sh
+++ images/faastion/build_container_image.sh
@@ -2,10 +2,6 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-GRAALVISOR_HOME=$ARGO_HOME/graalvisor
-LIB_DIR=$GRAALVISOR_HOME/shared/
-GRAALVISOR_BINARY=$GRAALVISOR_HOME/build/native-image/polyglot-proxy
-
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
@@ -18,33 +14,31 @@ fi
 DISK=$DIR/disk
 rm -rf $DISK &> /dev/null
 mkdir -p $DISK/
-# mkdir -p $DISK/graalvisor/build/native-image $DISK/$LIB_DIR
 
 echo '#!/bin/bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 if [ "$1" = "--enable-lpi" ]; then
+    export pku_isolation=on
     export LPI=true
 elif [ "$1" = "--enable-early-booking" ]; then
     export faastlane=true
 fi
 
-library_path="'$BUILD_HOME':'$LIBC_HOME'/lib:/usr/local/lib:/lib/x86_64-linux-gnu"
-env GLIBC_TUNABLES="glibc.rtld.nns=16" LD_LIBRARY_PATH="$library_path" LD_PRELOAD="libmem.so" $DIR/build/native-image/polyglot-proxy
+library_path=/faastion/graalvisor/shared:/glibc-2.35/build/install/lib:/lib/x86_64-linux-gnu:/usr/local/lib
+env GLIBC_TUNABLES="glibc.rtld.nns=16" LD_LIBRARY_PATH="$library_path" LD_PRELOAD=libmem.so ./faastion/graalvisor/build/native-image/polyglot-proxy
 ' > $DISK/start.sh
 
+# Create faastion zip file
 cd $ARGO_HOME
 git archive -o faastion.zip --prefix="faastion/" HEAD
 cd - &> /dev/null
 
-# Copy graalvisor and init.
+# Copy faastion repo and glibc
 cp $DIR/glibc-2.35.patch $DISK/.
 cp $DIR/glibc-2.35.tar.gz $DISK/.
 mv $ARGO_HOME/faastion.zip $DISK/.
-# cp $BUILD_HOME/*.so $DISK/$BUILD_HOME/.
-# cp -r $LIBC_HOME/* $DISK/$LIBC_HOME/.
-# cp $GRAALVISOR_BINARY $DISK/graalvisor/build/native-image/polyglot-proxy
 
 # Build docker.
 docker build -t faastion $DIR
