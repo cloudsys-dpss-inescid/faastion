f53ddab266bc0031e4dabc9a9cd2bc87f15956c4
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2025-10-17 19:19:34 +0100

    Create run scripts to help debug bfs

---
+++ benchmarks/src/java/graalvisor-runtime/gv-native-bfs/jvm_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NUM_THREADS=20
+ITER=100
+
+for i in $(seq 1 $NUM_THREADS)
+do
+  rm -f jvm-$i.out
+  touch jvm-$i.out
+done
+
+for i in $(seq 1 $NUM_THREADS)
+do
+  for n in $(seq 1 $ITER)
+  do
+    LD_LIBRARY_PATH=$ARGO_HOME/graalvisor/shared/ $DEF_JAVA_HOME/bin/java -cp build/libs/bfs-1.0-all.jar com.jni.BFS >> jvm-$i.out
+  done &
+done
+
+wait
+echo Finished
