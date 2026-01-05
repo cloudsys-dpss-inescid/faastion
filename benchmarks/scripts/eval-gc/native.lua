7e93aeefffbed27b8abca146001f437952320ec3
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2026-01-05 13:31:00 +0000

    Create scripts to evaluate the GC impact on knative/context isolation

---
+++ benchmarks/scripts/eval-gc/native.lua
@@ -0,0 +1,17 @@
+local socket = require("socket")
+
+local function_name = os.getenv("function_name")
+
+request = function()
+
+    wrk.headers["Content-Type"] = "application/json"
+    wrk.body = '{"name":"' .. function_name .. '","async":"false","cached":"true","arguments":"{}"}'
+    
+    return wrk.format("POST", nil, nil, wrk.body)
+end
+
+function response(status, headers, body)
+   print(body)
+end
+
+wrk.response = response
