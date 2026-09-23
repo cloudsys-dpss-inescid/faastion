2330e401e3a8f31292010ec0c263f08344ff8a12
Miguel Lourenço <miguel.lourenco2000@hotmail.com>
2026-09-23 01:33:24 +0100

    Update plot_memory.py

--- benchmarks/metrics/plot_memory.py
+++ benchmarks/metrics/plot_memory.py
@@ -10,6 +10,10 @@ import numpy as np
 if len(sys.argv) < 2:
 	sys.exit("Sytanx: " + sys.argv[0] + " <experiment_dir>")
 
+base_dir = sys.argv[1]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+plots_dir = os.path.join(dir_path, 'plots')
+
 EXPERIMENTS_DIR = sys.argv[1]
 
 BENCHMARKS = ["gv_bfs", "gv_mst", "gv_pagerank", "gv_compression", "gv_thumbnail", "gv_classify", "gv_dna", "gv_dynamic_html", "gv_uploader"]
@@ -108,10 +112,14 @@ def create_plot(metric, name, title, lbl):
     fig.legend(BASELINE_NAMES, loc='upper left', bbox_to_anchor=(0.1, 0.96))
     # fig.suptitle(title)
     plt.tight_layout()
-    plt.savefig(name + ".pdf", bbox_inches="tight")
-    plt.savefig(name + ".png", bbox_inches="tight")
+
+    if not os.path.exists(plots_dir):
+        os.makedirs(plots_dir)
+    
+    pdf_file = os.path.join(plots_dir, name + '.pdf')
+    png_file = os.path.join(plots_dir, name + '.png')
+    plt.savefig(pdf_file, bbox_inches="tight")
+    plt.savefig(png_file, bbox_inches="tight")
     plt.close()
 
-create_plot('tput', 'throughput', 'Throughput per Benchmark', "Throughput (Requests/sec)")
-create_plot('99p', 'tail_latency', 'Tail Latency per Benchmark', "99% Tail Latency (ms)")
 create_plot('memory', 'memory', 'Memory Footprint per Benchmark', "Memory Footprint (GB)")
